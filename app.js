10691809b2fd02f661af2e6e366f2cd312ee1f16
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-04-06 13:57:59 +0530

    Routed users/all

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
 
 const app = express();
+
+
 
 app.get('/', (req, res) => {
     res.send("Working fine Eshu");
 })
 
+app.get('/users/all', (req, res) => {
+    res.json({
+        success: true,
+        users: [],
+    })
+})
 app.listen(4000, () => {
     console.log('listening on port 4000');
 })
