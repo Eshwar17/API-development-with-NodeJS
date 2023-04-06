c925ece9b1024e59b6656f16897bdefec8677e1d
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-04-06 19:35:17 +0530

    Created APIs using NodeJS

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 
 const app = express();
+//using middlewares
+app.use(express.json());
 
 mongoose.connect("mongodb://127.0.0.1:27017", {
     dbName: 'backendapi',
@@ -21,14 +23,30 @@ const User = mongoose.model('User', schema);
 
 app.get('/', (req, res) => {
     res.send("Working fine Eshu");
-})
-
-app.get('/users/all', (req, res) => {
+});
+//getting Users API
+app.get('/users/all',async (req, res) => {
+    const users = await User.find({});
     res.json({
         success: true,
-        users: [],
-    })
-})
+        users,
+    });
+});
+
+//Posting Users API
+app.post("/users/new", async (req, res) => {
+    const {name, email, password} = req.body;
+
+    await User.create({
+        name, email, password
+    });
+
+    res.status(201).cookie("tempi", "lol").json({
+        success: true,
+        message: 'User created successfully',
+    });
+});
+
 app.listen(4000, () => {
     console.log('listening on port 4000');
 })
